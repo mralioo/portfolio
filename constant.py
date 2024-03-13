2407877e4e9d667a761b87db731c0b50a3fa3cf9
mralioo <ali.alouane@outlook.de>
2024-03-13 01:13:08 +0100

    add streamlit template portfolio webapp

---
+++ constant.py
@@ -0,0 +1,28 @@
+info = {
+  "Pronoun": "him",
+  "Name": "Ali",
+  "Full_Name": "Ali Alouane",
+  "Intro": "A Machine Learning Engineer",
+  "About": "Hey there, I'm ALI!  \n\n 🚀\nFrom zapping circuits with a Bachelor of Science in Electrical Engineering from the University of Stuttgart to mastering the art of AI at TU Berlin, my journey's been electric. I kicked off my professional adventure in the bustling world of logistics with Trumetrics, playing with algorithms instead of trucks, and then dabbled in automotive sorcery at IAV GmbH, where I juggled code and charm. 🧠\nMy Master's saga? \n\n It's peppered with brainy projects, like crafting a toolbox for neuroscience ninjas.\n\nAs a self-proclaimed MLOps Chef, I whip up automated ML pipelines that serve up AI solutions as if they were fast food - quick, efficient, and downright delicious. 🤖\nLately, I've been flirting with Large Language Models like GPT-4, dreaming up chatbots that could outwit your average toaster. My mission? \n\n To keep surfing the AI wave, updating my wizardry toolkit, and turning sci-fi into reality.\n\nWhen I'm not conjuring AI spells, you'll find me in the garage, building drones that could win races in the Matrix, or robocars that think they're in Fast & Furious. And when it's time to unplug, I hit the trails, jogging and hiking, because even wizards need to recharge in nature. Let's make magic happen together! ✨",
+  "City": "Berlin, Germany",
+  "Photo": "<a href=\"https://www.linkedin.com/in/ali-alouane/\"><img src=\"images/profile.jpg\" width=\"50\" alt=\"Profile\" title=\"Profile\"></a>",
+  "Email": "ali.alouane@outlook.de"
+}
+
+
+# embed_rss = {
+#     'rss': """<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed"
+#         data-rss="https://medium.com/feed/@vicky-note"
+#         data-maxcols="3"
+#         data-layout="grid"
+#         data-poststyle="inline"
+#         data-readmore="Read the rest"
+#         data-buttonclass="btn btn-primary"
+#         data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+# }
+#
+# endorsements = {
+#     "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
+#     "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
+#     "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
+# }
